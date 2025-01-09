54652154f3622a42114752634ed421a767195c41
HEX9CF <hex9cf@aliyun.com>
2025-01-09 18:09:47 +0800

    Refactor problem status

--- internal/entity/problem.go
+++ internal/entity/problem.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-// 难度：0 未知，1 入门，2 简单，3 中等，4 较难，5 困难，6 超难
+// Difficulty 难度：0 未知，1 入门，2 简单，3 中等，4 较难，5 困难，6 超难
 type Difficulty uint8
 
 const (
@@ -38,17 +38,7 @@ func (d Difficulty) String() string {
 	}
 }
 
-/*
-	状态
-
-1 作废
-2 出题
-3 调试
-4 隐藏
-5 比赛
-6 会员
-7 公开
-*/
+// ProblemStatus 题目状态: 1 作废, 2 出题, 3 调试, 4 隐藏, 5 比赛, 6 会员, 7 公开
 type ProblemStatus uint8
 
 const (
