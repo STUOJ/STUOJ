34380df5907e208e6c09a14ee8df5bcb80807003
HEX9CF <hex9cf@aliyun.com>
2025-01-10 16:38:12 +0800

    Update problem and tag gorm

--- internal/entity/problem_tag.go
+++ internal/entity/problem_tag.go
@@ -2,8 +2,11 @@ package entity
 
 // 题目标签关系
 type ProblemTag struct {
-	ProblemId uint64 `gorm:"primaryKey;autoIncrement:false;comment:题目ID"`
-	TagId     uint64 `gorm:"primaryKey;autoIncrement:false;comment:标签ID"`
+	ID        uint64  `gorm:"primaryKey;autoIncrement;comment:关系ID" json:"id"`
+	ProblemID uint64  `gorm:"not null;default:0;comment:题目ID" json:"problem_id"`
+	TagID     uint64  `gorm:"not null;default:0;comment:标签ID" json:"tag_id"`
+	Problem   Problem `gorm:"foreignKey:ProblemID;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE" json:"problem"`
+	Tag       Tag     `gorm:"foreignKey:TagID;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE" json:"tag"`
 }
 
 func (ProblemTag) TableName() string {
--- internal/entity/tag.go
+++ internal/entity/tag.go
@@ -2,9 +2,9 @@ package entity
 
 // 标签
 type Tag struct {
-	Id         uint64     `gorm:"primaryKey;autoIncrement;comment:标签ID" json:"id,omitempty"`
-	Name       string     `gorm:"type:varchar(255);not null;unique;default:'';comment:标签名" json:"name,omitempty"`
-	ProblemTag []*Problem `gorm:"many2many:tbl_problem_tag;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;association_jointable_foreignkey:problem_id;jointable_foreignkey:tag_id"`
+	Id       uint64    `gorm:"primaryKey;autoIncrement;comment:标签ID" json:"id,omitempty"`
+	Name     string    `gorm:"type:varchar(255);not null;unique;default:'';comment:标签名" json:"name,omitempty"`
+	Problems []Problem `gorm:"many2many:tbl_problem_tag;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE" json:"problems"`
 }
 
 func (Tag) TableName() string {
