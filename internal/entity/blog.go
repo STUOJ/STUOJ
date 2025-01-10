a09814a225b1a44e9e9695bd970dc3598befdabb
HEX9CF <hex9cf@aliyun.com>
2025-01-10 15:48:59 +0800

    Update blog gorm

--- internal/entity/blog.go
+++ internal/entity/blog.go
@@ -33,11 +33,11 @@ func (s BlogStatus) String() string {
 // 博客
 type Blog struct {
 	Id         uint64     `gorm:"primaryKey;autoIncrement;comment:'博客ID'" json:"id,omitempty"`
-	UserId     uint64     `gorm:"not null;default:0;comment:'用户ID'" json:"user_id,omitempty"`
-	ProblemId  uint64     `gorm:"not null;default:0;comment:'关联题目ID：0 不关联'" json:"problem_id,omitempty"`
+	UserId     uint64     `gorm:"not null;default:0;comment:'用户ID';constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;" json:"user_id,omitempty"`
+	ProblemId  uint64     `gorm:"not null;default:0;comment:'关联题目ID'" json:"problem_id,omitempty"`
 	Title      string     `gorm:"type:text;not null;comment:'标题'" json:"title,omitempty"`
 	Content    string     `gorm:"type:longtext;not null;comment:'内容'" json:"content,omitempty"`
-	Status     BlogStatus `gorm:"not null;default:1;comment:'状态：0 被屏蔽，1 公开，2 草稿'" json:"status"`
+	Status     BlogStatus `gorm:"not null;default:1;comment:'状态'" json:"status"`
 	CreateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:'创建时间'" json:"create_time,omitempty"`
 	UpdateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP;comment:'更新时间'" json:"update_time,omitempty"`
 }
