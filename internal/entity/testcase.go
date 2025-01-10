34adfbc1b377678ade0ae19b4db0679828a96fb9
HEX9CF <hex9cf@aliyun.com>
2025-01-10 16:42:06 +0800

    Update testcase gorm

--- internal/entity/testcase.go
+++ internal/entity/testcase.go
@@ -1,12 +1,13 @@
 package entity
 
-// 评测点数据
+// Testcase 测试用例
 type Testcase struct {
-	Id         uint64 `gorm:"primaryKey;autoIncrement;comment:评测点数据ID" json:"id,omitempty"`
-	Serial     uint64 `gorm:"not null;default:0;comment:评测点序号" json:"serial,omitempty"`
-	ProblemId  uint64 `gorm:"not null;default:0;comment:题目ID" json:"problem_id,omitempty"`
-	TestInput  string `gorm:"type:longtext;not null;comment:测试输入" json:"test_input,omitempty"`
-	TestOutput string `gorm:"type:longtext;not null;comment:测试输出" json:"test_output,omitempty"`
+	ID         uint64  `gorm:"primaryKey;autoIncrement;comment:评测点ID" json:"id"`
+	Serial     uint64  `gorm:"not null;default:0;comment:评测点序号" json:"serial"`
+	ProblemID  uint64  `gorm:"not null;default:0;comment:题目ID" json:"problem_id"`
+	TestInput  string  `gorm:"type:longtext;not null;comment:测试输入" json:"test_input"`
+	TestOutput string  `gorm:"type:longtext;not null;comment:测试输出" json:"test_output"`
+	Problem    Problem `gorm:"foreignKey:ProblemID;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:CASCADE" json:"problem"`
 }
 
 func (Testcase) TableName() string {
