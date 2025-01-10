9b4e239e2b4074cd209e9fe00eebf0ca7741c097
ArtdragonXoX <1262922587@qq.com>
2025-01-10 21:36:33 +0800

    feat(language): 增加通过 ID 查询语言的功能
    - 新增 SelectById 函数，用于根据 ID 查询语言信息
    - 优化代码结构，增加函数注释
    - 保持代码风格一致性

--- internal/service/language/select.go
+++ internal/service/language/select.go
@@ -25,6 +25,18 @@ func Select(condition dao.LanguageWhere, role entity.Role) ([]entity.Language, e
 	return languages, nil
 }
 
+func SelectById(id uint64) (entity.Language, error) {
+	var l entity.Language
+
+	l, err := dao.SelectLanguageById(id)
+	if err != nil {
+		log.Println(err)
+		return entity.Language{}, errors.New("查询语言失败")
+	}
+
+	return l, nil
+}
+
 // 根据名字模糊查询语言
 func SelectLikeName(name string) (entity.Language, error) {
 	var l entity.Language
