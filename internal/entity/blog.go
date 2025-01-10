9e762a61652e5f4ab8bcf4369d99aac6ae9304bc
HEX9CF <hex9cf@aliyun.com>
2025-01-10 16:16:00 +0800

    Update blog gorm

--- internal/entity/blog.go
+++ internal/entity/blog.go
@@ -30,16 +30,17 @@ func (s BlogStatus) String() string {
 	}
 }
 
-// 博客
+// Blog 博客
 type Blog struct {
-	Id         uint64     `gorm:"primaryKey;autoIncrement;comment:'博客ID'" json:"id,omitempty"`
-	UserId     uint64     `gorm:"not null;default:0;comment:'用户ID';constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;" json:"user_id,omitempty"`
-	ProblemId  uint64     `gorm:"not null;default:0;comment:'关联题目ID'" json:"problem_id,omitempty"`
-	Title      string     `gorm:"type:text;not null;comment:'标题'" json:"title,omitempty"`
-	Content    string     `gorm:"type:longtext;not null;comment:'内容'" json:"content,omitempty"`
-	Status     BlogStatus `gorm:"not null;default:1;comment:'状态'" json:"status"`
-	CreateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:'创建时间'" json:"create_time,omitempty"`
-	UpdateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP;comment:'更新时间'" json:"update_time,omitempty"`
+	Id         uint64     `gorm:"primaryKey;autoIncrement;comment:博客ID" json:"id"`
+	UserId     uint64     `gorm:"not null;default:0;comment:用户ID" json:"user_id"`
+	ProblemId  uint64     `gorm:"not null;default:0;comment:关联题目ID" json:"problem_id"`
+	Title      string     `gorm:"type:text;not null;comment:标题" json:"title"`
+	Content    string     `gorm:"type:longtext;not null;comment:内容" json:"content"`
+	Status     BlogStatus `gorm:"not null;default:1;comment:状态" json:"status"`
+	CreateTime time.Time  `gorm:"not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"create_time"`
+	UpdateTime time.Time  `gorm:"not null;default:CURRENT_TIMESTAMP;comment:更新时间" json:"update_time"`
+	User       User       `gorm:"foreignKey:UserId;references:Id" json:"user"`
 }
 
 func (Blog) TableName() string {
