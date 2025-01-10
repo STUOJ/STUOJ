3053e1bb9352227fbe844bb6eb0b00c694c9e507
HEX9CF <hex9cf@aliyun.com>
2025-01-10 16:25:26 +0800

    Update language gorm

--- internal/entity/language.go
+++ internal/entity/language.go
@@ -1,9 +1,12 @@
 package entity
 
-// 编程语言
+// Language 编程语言
 type Language struct {
-	Id   uint64 `gorm:"primaryKey;autoIncrement;comment:语言ID" json:"id,omitempty"`
-	Name string `gorm:"type:varchar(255);not null;comment:语言名" json:"name,omitempty"`
+	ID     uint64 `gorm:"primaryKey;autoIncrement;comment:语言ID" json:"id"`
+	Name   string `gorm:"type:varchar(255);not null;comment:语言名" json:"name"`
+	Serial uint64 `gorm:"not null;default:0;comment:排序序号" json:"serial"`
+	MapId  int    `gorm:"not null;default:0;comment:映射ID" json:"map_id"`
+	Status uint64 `gorm:"not null;default:3;comment:状态" json:"status"`
 }
 
 func (Language) TableName() string {
