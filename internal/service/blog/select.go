a964285e5684855545fe5c4641f3a1efd30e895c
ArtdragonXoX <1262922587@qq.com>
2025-01-13 20:51:28 +0800

    feat(blog): 优化博客查询功能
    - 修改 BlogWhere 结构体，将 UserId 和 ProblemId 字段改为 FieldList 类型
    - 更新 generateBlogWhereConditionWithNoPage 函数，支持多对多查询
    - 改进 Select 函数，根据用户角色设置查询条件
    - 在 handler 中实现多对多查询的参数解析

--- internal/service/blog/select.go
+++ internal/service/blog/select.go
@@ -26,9 +26,13 @@ func SelectById(id uint64, userId uint64, admin ...bool) (entity.Blog, error) {
 	return b, nil
 }
 
-func Select(condition dao.BlogWhere, userId uint64, admin ...bool) (BlogPage, error) {
-	if len(admin) == 0 || !admin[0] && (!condition.UserId.Exist() || condition.UserId.Value() != userId) {
-		condition.Status.Set(entity.BlogStatusPublic)
+func Select(condition dao.BlogWhere, userId uint64, role entity.Role) (BlogPage, error) {
+	if !condition.Status.Exist() && role < entity.RoleAdmin {
+		if !condition.UserId.Exist() || len(condition.UserId.Value()) != 1 || condition.UserId.Value()[0] != userId {
+			condition.Status.Set(entity.BlogStatusPublic)
+		}
+	} else if condition.Status.Value() < entity.BlogStatusPublic && role < entity.RoleAdmin {
+		condition.UserId.Set(userId)
 	}
 	if !condition.Page.Exist() {
 		condition.Page.Set(1)
