d9ef2d5edb32c1901e2b1d3dc7a6f9e794e1ecbe
HEX9CF <hex9cf@aliyun.com>
2025-01-13 18:00:33 +0800

    Add omitempty tag to MapId

--- internal/entity/language.go
+++ internal/entity/language.go
@@ -27,7 +27,7 @@ type Language struct {
 	Id     uint64         `gorm:"primaryKey;autoIncrement;comment:语言ID" json:"id"`
 	Name   string         `gorm:"type:varchar(255);not null;comment:语言名" json:"name"`
 	Serial uint16         `gorm:"not null;default:0;comment:排序序号" json:"serial"`
-	MapId  uint32         `gorm:"not null;default:0;comment:映射ID" json:"map_id"`
+	MapId  uint32         `gorm:"not null;default:0;comment:映射ID" json:"map_id,omitempty"`
 	Status LanguageStatus `gorm:"not null;default:3;comment:状态" json:"status"`
 }
 
