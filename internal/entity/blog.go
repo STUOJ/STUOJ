a6ec5400a6a417e627ef453939a95e3c6ab18978
HEX9CF <hex9cf@aliyun.com>
2025-01-16 23:16:05 +0800

    Update blog gorm

--- internal/entity/blog.go
+++ internal/entity/blog.go
@@ -38,7 +38,7 @@ type Blog struct {
 	CreateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"create_time"`
 	UpdateTime time.Time  `gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:更新时间" json:"update_time"`
 	User       User       `gorm:"foreignKey:UserId;references:Id;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT" json:"user"`
-	Problem    Problem    `json:"problem,omitempty"`
+	Problem    Problem    `gorm:"-" json:"problem,omitempty"`
 }
 
 func (Blog) TableName() string {
