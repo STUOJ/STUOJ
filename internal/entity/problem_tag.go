03ea656146a999b8cf02e6d125c827cb528ef493
ArtdragonXoX <1262922587@qq.com>
2025-01-17 18:22:01 +0800

    refactor(entity): 调整 ProblemTag 结构体设计
    - 移除自增的 Id 字段，使用 ProblemId 和 TagId 作为联合主键
    - 这种设计更符合多对多关系的表结构，可以提高数据完整性和查询效率

--- internal/entity/problem_tag.go
+++ internal/entity/problem_tag.go
@@ -2,9 +2,8 @@ package entity
 
 // 题目标签关系
 type ProblemTag struct {
-	Id        uint64  `gorm:"primaryKey;autoIncrement;comment:关系ID" json:"id"`
-	ProblemId uint64  `gorm:"not null;default:0;comment:题目ID" json:"problem_id"`
-	TagId     uint64  `gorm:"not null;default:0;comment:标签ID" json:"tag_id"`
+	ProblemId uint64  `gorm:"primaryKey;not null;default:0;comment:题目ID" json:"problem_id"`
+	TagId     uint64  `gorm:"primaryKey;not null;default:0;comment:标签ID" json:"tag_id"`
 	Problem   Problem `gorm:"foreignKey:ProblemId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"-"`
 	Tag       Tag     `gorm:"foreignKey:TagId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT" json:"-"`
 }
