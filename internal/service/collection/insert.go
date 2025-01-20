be15036c1a71ccac04aa39bbd59a4aec4d1ba5f6
HEX9CF <hex9cf@aliyun.com>
2025-01-20 20:47:53 +0800

    Update collection insert service

--- internal/service/collection/insert.go
+++ internal/service/collection/insert.go
@@ -5,16 +5,22 @@ import (
 	"STUOJ/internal/entity"
 	"errors"
 	"log"
+	"time"
 )
 
 // 插入题单
 func Insert(c entity.Collection) (uint64, error) {
 	var err error
 
+	updateTime := time.Now()
+	c.UpdateTime = updateTime
+	c.CreateTime = updateTime
+
+	// 插入题单
 	c.Id, err = dao.InsertCollection(c)
 	if err != nil {
 		log.Println(err)
-		return 0, errors.New("插入失败")
+		return 0, errors.New("插入题单失败")
 	}
 
 	return c.Id, nil
