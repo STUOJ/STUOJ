c001224a98623fa30bb992805387d511a4ebc1e4
HEX9CF <hex9cf@aliyun.com>
2025-01-20 20:45:43 +0800

    Update collection delete service

--- internal/service/collection/delete.go
+++ internal/service/collection/delete.go
@@ -2,24 +2,31 @@ package collection
 
 import (
 	"STUOJ/internal/dao"
+	"STUOJ/internal/entity"
 	"errors"
 	"log"
 )
 
 // 根据ID删除题单
-func DeleteById(id uint64) error {
+func Delete(id uint64, uid uint64, role entity.Role) error {
 	// 查询题单
-	_, err := dao.SelectCollectionById(id)
+	c0, err := dao.SelectBlogById(id)
 	if err != nil {
 		log.Println(err)
 		return errors.New("题单不存在")
+	}
+
+	if role < entity.RoleAdmin {
+		if c0.UserId != uid {
+			return errors.New("没有权限，只能删除自己的题单")
+		}
 	}
 
 	// 删除题单
 	err = dao.DeleteCollectionById(id)
 	if err != nil {
 		log.Println(err)
-		return errors.New("删除失败")
+		return errors.New("删除题单失败")
 	}
 
 	return nil
