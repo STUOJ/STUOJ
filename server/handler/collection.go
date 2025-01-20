9aa0914c3d2d329bd3cdcf54a25e5db085082ebc
HEX9CF <hex9cf@aliyun.com>
2025-01-20 20:10:49 +0800

    Add collection handlers

---
+++ server/handler/collection.go
@@ -0,0 +1,167 @@
+package handler
+
+import (
+	"STUOJ/internal/entity"
+	"STUOJ/internal/model"
+	"STUOJ/internal/service/collection"
+	"STUOJ/internal/service/problem"
+	"STUOJ/utils"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 获取题单列表
+func CollectionList(c *gin.Context) {
+
+	condition := model.CollectionWhere{}
+	condition.Parse(c)
+	users, err := collection.Select(condition)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	c.JSON(http.StatusOK, model.RespOk("OK", users))
+}
+
+// 添加题单
+type ReqCollectionAdd struct {
+	Name string `json:"name,omitempty" binding:"required"`
+}
+
+func CollectionAdd(c *gin.Context) {
+	var req ReqCollectionAdd
+
+	// 参数绑定
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	// 初始化题单
+	t := entity.Collection{
+		Name: req.Name,
+	}
+
+	// 插入题单
+	t.Id, err = collection.Insert(t)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, model.RespOk("添加成功，返回题单ID", t.Id))
+}
+
+// 修改题单数据
+type ReqCollectionModify struct {
+	Id   uint64 `json:"id,omitempty" binding:"required"`
+	Name string `json:"name,omitempty" binding:"required"`
+}
+
+func CollectionModify(c *gin.Context) {
+	var req ReqCollectionModify
+
+	// 参数绑定
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	err = collection.UpdateById(req.Id, req.Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, model.RespOk("修改成功", nil))
+}
+
+// 删除题单
+func CollectionRemove(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	// 删除题单
+	tid := uint64(id)
+	err = collection.DeleteById(tid)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, model.RespOk("删除成功", nil))
+}
+
+// 添加题单到题目
+type ReqProblemAddCollection struct {
+	ProblemId    uint64 `json:"problem_id,omitempty" binding:"required"`
+	CollectionId uint64 `json:"collection_id,omitempty" binding:"required"`
+}
+
+func ProblemAddCollection(c *gin.Context) {
+	role, uid := utils.GetUserInfo(c)
+	var req ReqProblemAddCollection
+
+	// 参数绑定
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	// 添加题单
+	err = problem.InsertCollection(req.ProblemId, req.CollectionId, uid, role)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, model.RespOk("添加成功", nil))
+}
+
+// 删除题目的某个题单
+type ReqProblemRemoveCollection struct {
+	ProblemId    uint64 `json:"problem_id,omitempty" binding:"required"`
+	CollectionId uint64 `json:"collection_id,omitempty" binding:"required"`
+}
+
+// 删除题目的某个题单
+func ProblemRemoveCollection(c *gin.Context) {
+	role, uid := utils.GetUserInfo(c)
+	var req ReqProblemRemoveCollection
+
+	// 参数绑定
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+
+	// 删除题单
+	err = problem.DeleteCollection(req.ProblemId, req.CollectionId, uid, role)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.RespError(err.Error(), nil))
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, model.RespOk("删除成功", nil))
+}
--- server/routes/problem.go
+++ server/routes/problem.go
@@ -75,7 +75,7 @@ func InitSolutionRoute(ginServer *gin.Engine) {
 func InitCollectionRoute(ginServer *gin.Engine) {
 	collectionPublicRoute := ginServer.Group("/collection")
 	{
-		collectionPublicRoute.GET("/", handler.TagList)
+		collectionPublicRoute.GET("/", handler.CollectionList)
 	}
 
 	collectionUserRoute := ginServer.Group("/tag")
@@ -83,8 +83,8 @@ func InitCollectionRoute(ginServer *gin.Engine) {
 		// 使用中间件
 		collectionUserRoute.Use(middlewares.TokenAuthUser())
 
-		collectionUserRoute.POST("/", handler.TagAdd)
-		collectionUserRoute.PUT("/", handler.TagModify)
-		collectionUserRoute.DELETE("/:id", handler.TagRemove)
+		collectionUserRoute.POST("/", handler.CollectionAdd)
+		collectionUserRoute.PUT("/", handler.CollectionModify)
+		collectionUserRoute.DELETE("/:id", handler.CollectionRemove)
 	}
 }
