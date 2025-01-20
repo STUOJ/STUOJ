f29015697cdd7f6725292bfa57dd419f2992d453
HEX9CF <hex9cf@aliyun.com>
2025-01-20 23:06:41 +0800

    Update collection delete service

--- internal/service/collection/delete.go
+++ internal/service/collection/delete.go
@@ -41,50 +41,37 @@ func DeleteProblem(cid uint64, pid uint64, uid uint64, role entity.Role) error {
 	}
 
 	// 读取题目
-	p0, err := dao.SelectProblemById(pid)
+	_, err := dao.SelectProblemById(pid)
 	if err != nil {
 		log.Println(err)
 		return errors.New("题目不存在")
 	}
 
+	// 读取题单
+	c0, err := dao.SelectCollectionById(cid)
+	if err != nil {
+		log.Println(err)
+		return errors.New("题单不存在")
+	}
+
 	if role < entity.RoleAdmin {
-		userIdsMap := make(map[uint64]struct{})
-		for _, uid := range p0.UserIds {
-			userIdsMap[uid] = struct{}{}
-		}
-		if _, exists := userIdsMap[uid]; !exists {
-			return errors.New("没有该题权限")
+		if c0.UserId != uid {
+			return errors.New("没有权限，只能操作自己的题单")
 		}
 	}
 
-	// 读取标签
-	_, err = dao.SelectTagById(cid)
-	if err != nil {
-		log.Println(err)
-		return errors.New("标签不存在")
-	}
-
-	// 检查题目标签关系是否存在
-	count, err := dao.CountProblemTag(cp)
-	if err != nil || count < 1 {
-		if err != nil {
-			log.Println(err)
-		}
-		return errors.New("该题目不存在该标签")
-	}
-
-	// 更新题目更新时间
-	err = dao.UpdateProblemUpdateTimeById(pid)
-	if err != nil {
-		log.Println(err)
-		return errors.New("更新题目更新时间失败")
-	}
-
-	// 删除题目标签
-	err = dao.DeleteProblemTag(cp)
+	// 删除题单题目
+	err = dao.DeleteCollectionProblem(cp)
 	if err != nil {
 		log.Println(err)
 		return errors.New("删除失败")
+	}
+
+	// 更新题单更新时间
+	err = dao.UpdateCollectionUpdateTimeById(pid)
+	if err != nil {
+		log.Println(err)
+		return errors.New("更新题单更新时间失败")
 	}
 
 	return nil
