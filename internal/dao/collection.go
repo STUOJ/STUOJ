08192f013be58c5b8a8dca869d770605bd6ad76e
ArtdragonXoX <1262922587@qq.com>
2025-01-21 13:02:57 +0800

    refactor(dao): 优化 SelectCollectionById 函数的查询条件
    - 将查询条件从直接使用 id 参数改为使用实体结构
    - 这种改动提高了代码的可读性和维护性

--- internal/dao/collection.go
+++ internal/dao/collection.go
@@ -36,7 +36,7 @@ func SelectCollectionById(id uint64) (entity.Collection, error) {
 
 	tx := db.Db.Model(&entity.Collection{})
 	tx = collectionJoins(tx)
-	tx = tx.Where("id = ?", id).
+	tx = tx.Where(&entity.Collection{Id: id}).
 		Scan(&c)
 
 	if tx.Error != nil {
