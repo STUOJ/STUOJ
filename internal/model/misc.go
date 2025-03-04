0c37eb7ab67a31cb0ddd4a63b3a58dbc40eeaa29
HEX9CF <hex9cf@aliyun.com>
2025-03-04 17:11:05 +0800

    feat(model): add CommentWhere, SubmissionWhere, and TagWhere structures with parsing and querying methods

--- internal/model/misc.go
+++ internal/model/misc.go
@@ -2,8 +2,11 @@ package model
 
 import (
 	"STUOJ/utils"
+	"errors"
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -108,4 +111,67 @@ type ConditonWhere interface {
 	Parse(c gin.Context)
 	GenerateWhere() func(gorm.DB) gorm.DB
 	GenerateWhereWithNoPage() func(gorm.DB) gorm.DB
+}
+
+// 时间范围
+type Period struct {
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
+}
+
+// 检查时间范围
+func (p *Period) Check() error {
+	if p.StartTime.After(p.EndTime) {
+		return errors.New("开始时间不能晚于结束时间")
+	}
+	return nil
+}
+
+// 从字符串解析时间范围
+func (p *Period) FromString(startTimeStr string, endTimeStr string, layout string) error {
+	var err error
+
+	if startTimeStr == "" || endTimeStr == "" {
+		return errors.New("参数错误")
+	}
+
+	location, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return errors.New("加载时区失败")
+	}
+
+	p.StartTime, err = time.ParseInLocation(layout, startTimeStr, location)
+	if err != nil {
+		log.Println(err)
+		return errors.New("开始时间格式错误")
+	}
+	p.EndTime, err = time.ParseInLocation(layout, endTimeStr, location)
+	if err != nil {
+		log.Println(err)
+		return errors.New("结束时间格式错误")
+	}
+
+	return nil
+}
+
+func (p *Period) GetPeriod(c *gin.Context) error {
+	var err error
+
+	// 读取参数
+	startTimeStr := c.Query("start-time")
+	endTimeStr := c.Query("end-time")
+
+	// 解析时间范围
+	err = p.FromString(startTimeStr, endTimeStr, utils.DATETIME_LAYOUT)
+	if err != nil {
+		return err
+	}
+
+	// 检查时间范围
+	err = p.Check()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/model/problem.go
+++ internal/model/problem.go
@@ -141,3 +141,62 @@ func briefProblemJoins(db *gorm.DB, tbl string) *gorm.DB {
 	db = db.Joins(fmt.Sprintf("LEFT JOIN tbl_problem ON %s.problem_id = tbl_problem.id", tbl))
 	return db
 }
+
+type SolutionWhere struct {
+}
+
+func (con *SolutionWhere) GenerateWhere() func(*gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		whereClause := map[string]interface{}{}
+		where := db.Where(whereClause)
+		query := []string{"tbl_solution.*"}
+		return where.Select(query)
+	}
+}
+
+type HistoryWhere struct {
+}
+
+func (con *HistoryWhere) GenerateWhere() func(*gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		whereClause := map[string]interface{}{}
+		where := db.Where(whereClause)
+		query := []string{"tbl_history.*"}
+		query = append(query, briefUserSelect()...)
+		where = briefUserJoins(where, "tbl_history")
+		return where.Select(query)
+	}
+}
+
+type TagWhere struct {
+	Name Field[string]
+	Page Field[uint64]
+	Size Field[uint64]
+}
+
+func (con *TagWhere) Parse(c *gin.Context) {
+	con.Name.Parse(c, "name")
+	con.Page.Parse(c, "page")
+	con.Size.Parse(c, "size")
+}
+
+func (con *TagWhere) GenerateWhereWithNoPage() func(*gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		whereClause := map[string]interface{}{}
+		where := db.Where(whereClause)
+		if con.Name.Exist() {
+			where = where.Where("tbl_tag.title LIKE ?", "%"+con.Name.Value()+"%")
+		}
+		return where
+	}
+}
+
+func (con *TagWhere) GenerateWhere() func(*gorm.DB) *gorm.DB {
+	where := con.GenerateWhereWithNoPage()
+	return func(db *gorm.DB) *gorm.DB {
+		if con.Page.Exist() && con.Size.Exist() {
+			return where(db).Offset(int((con.Page.Value() - 1) * con.Size.Value())).Limit(int(con.Size.Value()))
+		}
+		return where(db).Offset(0).Limit(1)
+	}
+}
