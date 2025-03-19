9570d4239dc49f39b8230cf49b136511155030ad
HEX9CF <hex9cf@aliyun.com>
2025-03-19 10:59:27 +0800

    feat(team): implement team insertion and user management functions

---
+++ internal/service/team/insert.go
@@ -0,0 +1,65 @@
+package team
+
+import (
+	"STUOJ/internal/dao"
+	"STUOJ/internal/entity"
+	"errors"
+	"log"
+	"time"
+)
+
+// 插入团队
+func Insert(c entity.Team) (uint64, error) {
+	var err error
+
+	updateTime := time.Now()
+	c.UpdateTime = updateTime
+	c.CreateTime = updateTime
+
+	// 插入团队
+	c.Id, err = dao.InsertTeam(c)
+	if err != nil {
+		log.Println(err)
+		return 0, errors.New("插入失败")
+	}
+
+	return c.Id, nil
+}
+
+func InsertUser(tid uint64, addUid uint64, uid uint64, role entity.Role) error {
+	cu := entity.TeamUser{
+		TeamId: tid,
+		UserId: addUid,
+	}
+
+	_, err := dao.SelectUserById(addUid)
+	if err != nil {
+		log.Println(err)
+		return errors.New("用户不存在")
+	}
+
+	t0, err := dao.SelectTeamById(tid)
+	if err != nil {
+		log.Println(err)
+		return errors.New("团队不存在")
+	}
+	if role < entity.RoleAdmin {
+		if t0.UserId != uid {
+			return errors.New("没有权限，只能操作自己的团队")
+		}
+	}
+	if t0.UserId == addUid {
+		return errors.New("不能添加自己为团队成员")
+	}
+	for _, i := range t0.UserIds {
+		if i == addUid {
+			return errors.New("用户已存在团队中")
+		}
+	}
+	err = dao.InsertTeamUser(cu)
+	if err != nil {
+		log.Println(err)
+		return errors.New("添加用户失败")
+	}
+	return nil
+}
