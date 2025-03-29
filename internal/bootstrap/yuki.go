dd876d5ad08289d452ee64efb859920fa0b63048
ArtDragonXoX <1262922587@qq.com>
2025-03-29 17:47:00 +0800

    refactor: 将YukiImage初始化参数改为从配置中读取
    将yuki.InitYukiImage的调用改为从配置文件中读取host、port和token参数，提高代码的可配置性和可维护性。

--- internal/bootstrap/yuki.go
+++ internal/bootstrap/yuki.go
@@ -2,11 +2,12 @@ package bootstrap
 
 import (
 	"STUOJ/external/yuki"
+	"STUOJ/internal/conf"
 	"log"
 )
 
 func initYuki() {
-	err := yuki.InitYukiImage()
+	err := yuki.InitYukiImage(conf.Conf.YukiImage.Host, conf.Conf.YukiImage.Port, conf.Conf.YukiImage.Token)
 	if err != nil {
 		log.Println(err)
 		log.Println("初始化 yuki-image 失败！")
