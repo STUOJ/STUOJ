943e30e2436f1ae499ac1d0e8b65e8dbe5c87625
ArtDragonXoX <1262922587@qq.com>
2025-04-12 14:52:51 +0800

    feat(db/query): 添加Selector结构体及其相关方法
    新增Selector结构体用于构建查询选项，包含Query和Arg字段，并提供了添加参数、设置查询语句、获取选中列等方法。同时引入了Selectors类型以支持多个Selector的管理和字符串化操作。

---
+++ internal/db/query/option/select.go
@@ -0,0 +1,45 @@
+package option
+
+import "fmt"
+
+type Selector struct {
+	Query string
+	Arg   []any
+}
+
+func (s *Selector) SelectedColumns() []any {
+	return s.Arg
+}
+
+func (s *Selector) AddArg(arg ...any) {
+	s.Arg = append(s.Arg, arg...)
+}
+
+func (s *Selector) SetQuery(query string) {
+	s.Query = query
+}
+
+func (s *Selector) String() string {
+	return fmt.Sprintf(s.Query, s.Arg...)
+}
+
+func NewSelector(query string, arg ...any) *Selector {
+	return &Selector{
+		Query: query,
+		Arg:   arg,
+	}
+}
+
+type Selectors []Selector
+
+func (s *Selectors) AddSelector(selector ...Selector) {
+	*s = append(*s, selector...)
+}
+
+func (s *Selectors) String() string {
+	var str string
+	for _, selector := range *s {
+		str += selector.String() + ","
+	}
+	return str[:len(str)-1]
+}
