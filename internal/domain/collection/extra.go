8ba8e2cf21dc28d38ed61ee2e3adcc62f77b9779
ArtDragonXoX <1262922587@qq.com>
2025-04-17 18:25:28 +0800

    refactor(collection): 重命名查询常量并提取查询逻辑到函数中
    为了提高代码的可读性和可维护性，将查询常量重命名为更具描述性的名称，并将查询逻辑提取到独立的函数中。具体包括将 `CollectionProblem` 重命名为 `QueryCollectionProblem` 和 `WhereCollectionProblem`，并新增 `QueryProblemId` 和 `QueryUserId` 函数来封装查询逻辑。

--- internal/domain/collection/extra.go
+++ internal/domain/collection/extra.go
@@ -3,13 +3,39 @@ package collection
 import "STUOJ/internal/db/query/option"
 
 const (
-	CollectionProblem = "tbl_collection.id IN (SELECT collection_id FROM tbl_collection_problem WHERE problem_id In(?) GROUP BY collection_id HAVING COUNT(DISTINCT problem_id) =?)"
+	QueryCollectionProblem = "(SELECT GROUP_CONCAT(DISTINCT tbl_collection_problem.problem_id ORDER BY serial ASC) FROM tbl_collection_problem WHERE tbl_collection_problem.collection_id = tbl_collection.id) AS collection_problemIds"
+)
+
+func QueryProblemId() option.QueryContextOption {
+	return func(pqm option.QueryContext) option.QueryContext {
+		field := pqm.GetField()
+		selector := option.NewSelector(QueryCollectionProblem, option.OpExtra)
+		field.AddSelect(*selector)
+		return pqm
+	}
+}
+
+const (
+	QueryCollectionUser = "(SELECT GROUP_CONCAT(DISTINCT tbl_collection_user.user_id) FROM tbl_collection_user WHERE tbl_collection_user.collection_id = tbl_collection.id) AS collection_userIds"
+)
+
+func QueryUserId() option.QueryContextOption {
+	return func(pqm option.QueryContext) option.QueryContext {
+		field := pqm.GetField()
+		selector := option.NewSelector(QueryCollectionUser, option.OpExtra)
+		field.AddSelect(*selector)
+		return pqm
+	}
+}
+
+const (
+	WhereCollectionProblem = "tbl_collection.id IN (SELECT collection_id FROM tbl_collection_problem WHERE problem_id In(?) GROUP BY collection_id HAVING COUNT(DISTINCT problem_id) =?)"
 )
 
 func WhereProblem(ids []int64) option.QueryContextOption {
 	return func(context option.QueryContext) option.QueryContext {
 		filter := context.GetExtraFilters()
-		filter.Add(CollectionProblem, option.OpExtra, ids, len(ids))
+		filter.Add(WhereCollectionProblem, option.OpExtra, ids, len(ids))
 		return context
 	}
 }
