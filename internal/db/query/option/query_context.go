d2b6f00641d43b0af7d766719a9e7c1f4cf74b0f
ArtDragonXoX <1262922587@qq.com>
2025-04-17 10:13:06 +0800

    feat(query): 添加获取额外过滤器的接口
    为QueryContext接口新增GetExtraFilters方法，以便在查询上下文中获取额外的过滤器。同时，添加QueryParams结构体，用于封装查询参数，包括额外过滤器、分页和排序信息。

--- internal/db/query/option/query_context.go
+++ internal/db/query/option/query_context.go
@@ -3,6 +3,13 @@ package option
 type QueryContext interface {
 	GenerateOptions() *QueryOptions
 	GetField() FieldSelector
+	GetExtraFilters() *Filters
 }
 
 type QueryContextOption func(QueryContext) QueryContext
+
+type QueryParams struct {
+	ExtraFilters Filters
+	Page         Pagination
+	Sort         Sort
+}
