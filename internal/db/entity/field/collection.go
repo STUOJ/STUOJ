ebcd0215ffdf6beb678e89c5f05d22b51cbb64ac
ArtDragonXoX <1262922587@qq.com>
2025-04-17 10:33:28 +0800

    refactor(db): 删除未使用的集合问题查询常量
    删除 `internal/db/entity/field/collection.go` 文件中的 `CollectionProblem` 常量，因其在当前代码库中未被使用，以简化代码结构并减少冗余。

--- internal/db/entity/field/collection.go
+++
@@ -1,5 +0,0 @@
-package field
-
-var (
-	CollectionProblem = "tbl_collection.id IN (SELECT collection_id FROM tbl_collection_problem WHERE problem_id In(?) GROUP BY collection_id HAVING COUNT(DISTINCT problem_id) =?)"
-)
