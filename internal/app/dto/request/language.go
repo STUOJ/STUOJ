48c40453137f62da58a2006f0a341f8c745ae986
ArtDragonXoX <1262922587@qq.com>
2025-04-18 18:58:23 +0800

    refactor(dto): 将QueryLanguageParams的Status字段类型从int64改为string
    将QueryLanguageParams结构体中的Status字段类型从int64改为string，以更好地适应业务需求。同时，移除了UpdateLanguageReq结构体中未使用的Name字段，简化了数据结构。

--- internal/app/dto/request/language.go
+++ internal/app/dto/request/language.go
@@ -3,13 +3,12 @@ package request
 type QueryLanguageParams struct {
 	Order   *string `form:"order,omitempty"`
 	OrderBy *string `form:"order_by,omitempty"`
-	Status  *int64  `form:"status,omitempty"`
+	Status  *string `form:"status,omitempty"`
 }
 
 type UpdateLanguageReq struct {
-	ID     int64  `json:"id"`
-	MapID  int64  `json:"map_id,omitempty"`
-	Name   string `json:"name"`
-	Serial int64  `json:"serial"`
-	Status int64  `json:"status"`
+	ID     int64 `json:"id"`
+	MapID  int64 `json:"map_id,omitempty"`
+	Serial int64 `json:"serial"`
+	Status int64 `json:"status"`
 }
