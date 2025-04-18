5987f902dc6042330b3e395ce5ef8a6217af065d
HEX9CF <hex9cf@aliyun.com>
2025-04-18 23:27:56 +0800

    feat(blog): implement builder pattern for Blog creation with flexible options

---
+++ internal/domain/blog/builder.go
@@ -0,0 +1,54 @@
+package blog
+
+import (
+	"STUOJ/internal/db/entity"
+	"STUOJ/internal/domain/blog/valueobject"
+)
+
+type Option func(*Blog)
+
+func NewBlog(option ...Option) *Blog {
+	b := &Blog{
+		Status: entity.BlogDraft,
+	}
+	for _, opt := range option {
+		opt(b)
+	}
+	return b
+}
+
+func WithId(id uint64) Option {
+	return func(b *Blog) {
+		b.Id = id
+	}
+}
+
+func WithUserId(userId uint64) Option {
+	return func(b *Blog) {
+		b.UserId = userId
+	}
+}
+
+func WithProblemId(problemId uint64) Option {
+	return func(b *Blog) {
+		b.ProblemId = problemId
+	}
+}
+
+func WithTitle(title string) Option {
+	return func(b *Blog) {
+		b.Title = valueobject.NewTitle(title)
+	}
+}
+
+func WithContent(content string) Option {
+	return func(b *Blog) {
+		b.Content = valueobject.NewContent(content)
+	}
+}
+
+func WithStatus(status entity.BlogStatus) Option {
+	return func(b *Blog) {
+		b.Status = status
+	}
+}
