0a5abc557a03b7c752f29348a0385b90396e86f0
HEX9CF <hex9cf@aliyun.com>
2025-04-19 09:59:14 +0800

    feat(image): implement builder pattern for Image creation with flexible options

--- internal/domain/image/image.go
+++ internal/domain/image/image.go
@@ -53,31 +53,3 @@ func (i Image) Delete() error {
 	}
 	return &errors.NoError
 }
-
-type Option func(*Image)
-
-func NewImage(options ...Option) Image {
-	image := Image{}
-	for _, option := range options {
-		option(&image)
-	}
-	return image
-}
-
-func WithReader(reader io.Reader) Option {
-	return func(i *Image) {
-		i.Reader = reader
-	}
-}
-
-func WithKey(key string) Option {
-	return func(i *Image) {
-		i.Key = key
-	}
-}
-
-func WithAlbum(album uint8) Option {
-	return func(i *Image) {
-		i.Album = valueobject.Album(album)
-	}
-}
