d83281395e80b85c79a16a74114bd34dd9e41c04
HEX9CF <hex9cf@aliyun.com>
2025-04-19 09:30:47 +0800

    feat(model): refactor and reorganize model structures into separate files for better maintainability

---
+++ internal/model/request.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"STUOJ/internal/db/entity"
+	"github.com/gin-gonic/gin"
+)
+
+type ReqUser struct {
+	Id   uint64
+	Role entity.Role
+}
+
+func (r *ReqUser) Parse(c *gin.Context) {
+	role, exist := c.Get("req_user_id")
+	if !exist {
+		role = entity.RoleVisitor
+	}
+	id, exist := c.Get("req_user_role")
+	if !exist {
+		id = 0
+	}
+	r.Id = id.(uint64)
+	r.Role = role.(entity.Role)
+}
+
+func NewReqUser(c *gin.Context) *ReqUser {
+	r := &ReqUser{}
+	r.Parse(c)
+	return r
+}
---
+++ internal/model/time.go
@@ -0,0 +1,72 @@
+package model
+
+import (
+	"STUOJ/utils"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
+
+// 时间范围
+type Period struct {
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
+}
+
+// 检查时间范围
+func (p *Period) Check() error {
+	if p.StartTime.After(p.EndTime) {
+		return errors.New("开始时间不能晚于结束时间")
+	}
+	return nil
+}
+
+// 从字符串解析时间范围
+func (p *Period) FromString(startTimeStr string, endTimeStr string, layout string) error {
+	var err error
+
+	if startTimeStr == "" || endTimeStr == "" {
+		return errors.New("参数错误")
+	}
+
+	location, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return errors.New("加载时区失败")
+	}
+
+	p.StartTime, err = time.ParseInLocation(layout, startTimeStr, location)
+	if err != nil {
+		log.Println(err)
+		return errors.New("开始时间格式错误")
+	}
+	p.EndTime, err = time.ParseInLocation(layout, endTimeStr, location)
+	if err != nil {
+		log.Println(err)
+		return errors.New("结束时间格式错误")
+	}
+
+	return nil
+}
+
+func (p *Period) GetPeriod(c *gin.Context) error {
+	var err error
+
+	// 读取参数
+	startTimeStr := c.Query("start-time")
+	endTimeStr := c.Query("end-time")
+
+	// 解析时间范围
+	err = p.FromString(startTimeStr, endTimeStr, utils.DATETIME_LAYOUT)
+	if err != nil {
+		return err
+	}
+
+	// 检查时间范围
+	err = p.Check()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
