0ecf3324dc750ec4dd5ea3e7ff2f1b301925c70f
HEX9CF <hex9cf@aliyun.com>
2025-04-22 16:19:06 +0800

    feat(tag): add builder pattern for tag creation with options

--- internal/domain/tag/tag.go
+++ internal/domain/tag/tag.go
@@ -82,25 +82,3 @@ func (t *Tag) Delete() error {
 	}
 	return &errors.NoError
 }
-
-type Option func(*Tag)
-
-func NewTag(options ...Option) *Tag {
-	tag := &Tag{}
-	for _, option := range options {
-		option(tag)
-	}
-	return tag
-}
-
-func WithId(id int64) Option {
-	return func(t *Tag) {
-		t.Id = id
-	}
-}
-
-func WithName(name string) Option {
-	return func(t *Tag) {
-		t.Name = valueobject.NewName(name)
-	}
-}
