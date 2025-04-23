16950b3858f72e9fbd7d9de34b66a4a50ab6c98e
ArtDragonXoX <1262922587@qq.com>
2025-04-23 11:30:47 +0800

    refactor(dto/request): 移除QuerySubmissionParams中未使用的字段
    移除`Distinct`和`ExcludeHistory`字段，以简化代码结构并减少冗余

--- internal/app/dto/request/submission.go
+++ internal/app/dto/request/submission.go
@@ -1,16 +1,14 @@
 package request
 
 type QuerySubmissionParams struct {
-	Distinct       *string `form:"distinct,omitempty"`
-	EndTime        *string `form:"end-time,omitempty"`
-	ExcludeHistory *bool   `form:"exclude_history,omitempty"`
-	Language       *int64  `form:"language,omitempty"`
-	Order          *string `form:"order,omitempty"`
-	OrderBy        *string `form:"order_by,omitempty"`
-	Page           *int64  `form:"page,omitempty"`
-	Problem        *int64  `form:"problem,omitempty"`
-	Size           *int64  `form:"size,omitempty"`
-	StartTime      *string `form:"start-time,omitempty"`
-	Status         *int64  `form:"status,omitempty"`
-	User           *int64  `form:"user,omitempty"`
+	EndTime   *string `form:"end-time,omitempty"`
+	Language  *int64  `form:"language,omitempty"`
+	Order     *string `form:"order,omitempty"`
+	OrderBy   *string `form:"order_by,omitempty"`
+	Page      *int64  `form:"page,omitempty"`
+	Problem   *int64  `form:"problem,omitempty"`
+	Size      *int64  `form:"size,omitempty"`
+	StartTime *string `form:"start-time,omitempty"`
+	Status    *int64  `form:"status,omitempty"`
+	User      *int64  `form:"user,omitempty"`
 }
