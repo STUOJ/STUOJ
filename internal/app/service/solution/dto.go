ab964dd457dfbd0004e231e4881265a8eefe4b01
ArtDragonXoX <1262922587@qq.com>
2025-04-24 16:13:59 +0800

    feat(解决方案): 添加统计和分页查询功能
    新增 `GetStatistics` 函数用于统计解决方案数量，并扩展 `Select` 函数以支持分页查询。通过 `params2Query` 函数将请求参数转换为查询条件，提升查询灵活性和代码复用性。

--- internal/app/service/solution/dto.go
+++ internal/app/service/solution/dto.go
@@ -1,9 +1,30 @@
 package solution
 
 import (
+	"STUOJ/internal/app/dto/request"
 	"STUOJ/internal/app/dto/response"
+	"STUOJ/internal/db/query/option"
+	"STUOJ/internal/db/querycontext"
 	"STUOJ/internal/domain/solution"
+	"STUOJ/utils"
 )
+
+func params2Query(params request.QuerySolutionParams) (query querycontext.SolutionQueryContext) {
+	if params.Language != nil {
+		if ids, err := utils.StringToInt64Slice(*params.Language); err == nil {
+			query.LanguageId.Set(ids)
+		}
+	}
+	if params.Problem != nil {
+		if ids, err := utils.StringToInt64Slice(*params.Problem); err == nil {
+			query.ProblemId.Set(ids)
+		}
+	}
+	if params.Page != nil && params.Size != nil {
+		query.Page = option.NewPagination(*params.Page, *params.Size)
+	}
+	return query
+}
 
 func domain2Resp(dm solution.Solution) response.SolutionData {
 	return response.SolutionData{
--- internal/app/service/solution/select.go
+++ internal/app/service/solution/select.go
@@ -1,11 +1,36 @@
 package solution
 
 import (
+	"STUOJ/internal/app/dto/request"
 	"STUOJ/internal/app/dto/response"
+	"STUOJ/internal/db/query"
 	"STUOJ/internal/db/querycontext"
 	"STUOJ/internal/domain/solution"
 	"STUOJ/internal/model"
 )
+
+type SolutionPage struct {
+	Solutions []response.SolutionData `json:"solutions"`
+	model.Page
+}
+
+func Select(params request.QuerySolutionParams, reqUser model.ReqUser) (SolutionPage, error) {
+	var res SolutionPage
+	query_ := params2Query(params)
+	query_.Field = *query.SolutionAllField
+	solutions, _, err := solution.Query.Select(query_)
+	if err != nil {
+		return res, err
+	}
+	for _, s := range solutions {
+		res.Solutions = append(res.Solutions, domain2Resp(s))
+	}
+	res.Page.Page = query_.Page.Page
+	res.Page.Size = query_.Page.PageSize
+	total, _ := GetStatistics(params)
+	res.Page.Total = total
+	return res, nil
+}
 
 // SelectById 根据ID查询评测点数据
 func SelectById(id int64, reqUser model.ReqUser) (response.SolutionData, error) {
