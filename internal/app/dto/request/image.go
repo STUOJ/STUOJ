cf46416ab2867bda8bcd42dfea5c3149249aac3c
ArtDragonXoX <1262922587@qq.com>
2025-04-26 15:25:33 +0800

    refactor(image): 将角色字段类型从字符串改为int64并重构图片上传逻辑
    将 `UploadImageReq` 中的 `Role` 字段类型从 `string` 改为 `int64`，以更好地匹配业务逻辑。同时，重构了 `UploadImage` 处理函数，简化了代码结构并移除了不必要的依赖。

--- internal/app/dto/request/image.go
+++ internal/app/dto/request/image.go
@@ -3,6 +3,6 @@ package request
 import "mime/multipart"
 
 type UploadImageReq struct {
-	Role string                `form:"role"`
+	Role int64                 `form:"role"`
 	File *multipart.FileHeader `form:"file"`
 }
--- server/handler/misc.go
+++ server/handler/misc.go
@@ -1,46 +1,35 @@
 package handler
 
 import (
-	"STUOJ/external/yuki"
+	"STUOJ/internal/app/dto/request"
+	"STUOJ/internal/app/service/image"
+	"STUOJ/internal/errors"
 	"STUOJ/internal/model"
 	"STUOJ/utils"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UploadImage(c *gin.Context) {
-	role, err := strconv.Atoi(c.Query("role"))
-	if err != nil || model.GetAlbumName(uint8(role)) == "unknown" {
-		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
-		return
-	}
-	file, err := c.FormFile("file")
+	reqUser := model.NewReqUser(c)
+	var req request.UploadImageReq
+	err := c.ShouldBind(&req)
 	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, model.RespError("文件上传失败", nil))
+		c.Error(&errors.ErrValidation)
 		return
 	}
-
-	// 保存文件
-	dst := fmt.Sprintf("tmp/%s", utils.GetRandKey(6))
-	if err := c.SaveUploadedFile(file, dst); err != nil {
-		log.Println(err)
-		c.JSON(http.StatusBadRequest, model.RespError("文件上传失败", nil))
-		return
-	}
-	defer os.Remove(dst)
-
-	image, err := yuki.UploadImage(dst, uint8(role))
+	req.File, err = c.FormFile("file")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, model.RespError("上传失败", nil))
+		c.Error(&errors.ErrValidation)
 		return
 	}
-	c.JSON(http.StatusOK, model.RespOk("上传成功", image.Url))
+	url, err := image.Insert(req, *reqUser)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	c.JSON(http.StatusOK, model.RespOk("上传成功", url))
 }
 
 type ReqEmail struct {
