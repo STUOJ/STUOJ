3b67f28bb0c6ce495207b47fa48fbbbf5c44b7cf
ArtDragonXoX <1262922587@qq.com>
2025-04-29 15:41:03 +0800

    refactor: 移除统计相关路由和处理器代码
    清理不再使用的统计功能路由和处理器代码，以简化代码库并提高可维护性

--- server/routes/statistics.go
+++ server/routes/statistics.go
@@ -1,30 +1,9 @@
 package routes
 
 import (
-	"STUOJ/server/handler"
-
 	"github.com/gin-gonic/gin"
 )
 
 func InitStatisticsRoute(ginServer *gin.Engine) {
-	statisticsPublicRoute := ginServer.Group("/statistics")
-	{
-		statisticsPublicRoute.GET("/user", handler.StatisticsUser)
-		statisticsPublicRoute.GET("/user/role", handler.StatisticsUserOfRole)
-		statisticsPublicRoute.GET("/user/register", handler.StatisticsUserOfRegister)
 
-		statisticsPublicRoute.GET("/tag", handler.StatisticsTag)
-		statisticsPublicRoute.GET("/problem", handler.StatisticsProblem)
-		statisticsPublicRoute.GET("/problem/insert", handler.StatisticsProblemOfInsert)
-		statisticsPublicRoute.GET("/problem/update", handler.StatisticsProblemOfUpdate)
-		statisticsPublicRoute.GET("/problem/delete", handler.StatisticsProblemOfDelete)
-
-		statisticsPublicRoute.GET("/judge", handler.StatisticsJudge)
-
-		statisticsPublicRoute.GET("/record", handler.StatisticsRecord)
-
-		statisticsPublicRoute.GET("/blog", handler.StatisticsBlog)
-		statisticsPublicRoute.GET("/blog/submit", handler.StatisticsBlogOfSubmit)
-		statisticsPublicRoute.GET("/comment/submit", handler.StatisticsCommentOfSubmit)
-	}
 }
