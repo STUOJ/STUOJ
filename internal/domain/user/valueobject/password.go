f0b3a338899d1ea1723c7874e324637898c4efbd
HEX9CF <hex9cf@aliyun.com>
2025-05-01 22:54:37 +0800

    refactor: restructure Password type to encapsulate plaintext and ciphertext, enhance hashing and verification methods

--- internal/domain/user/valueobject/password.go
+++ internal/domain/user/valueobject/password.go
@@ -6,33 +6,53 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Password string
+type Password struct {
+	plaintext  string
+	ciphertext string
+}
 
 func (p Password) Verify() error {
-	if len(p) < 6 || len(p) > 20 {
+	if len(p.plaintext) < 6 || len(p.plaintext) > 20 {
 		return errors.New("密码长度必须在6-20个字符之间！")
 	}
 	return nil
 }
 
 func (p Password) String() string {
-	return string(p)
+	return p.plaintext
+}
+
+func hash(plaintext string) (string, error) {
+	ciphertext, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(ciphertext), nil
 }
 
 // Hash 密码加密
 func (p Password) Hash() (Password, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
+	var err error
+
+	if p.plaintext == "" {
+		return Password{}, errors.New("密码不能为空")
 	}
-	return Password(hashedPassword), nil
+	p.ciphertext, err = hash(p.plaintext)
+	if err != nil {
+		return Password{}, err
+	}
+
+	return p, nil
 }
 
 // VerifyHash 验证密码
 func (p Password) VerifyHash(pw string) error {
-	return bcrypt.CompareHashAndPassword([]byte(p), []byte(pw))
+	return bcrypt.CompareHashAndPassword([]byte(p.ciphertext), []byte(pw))
 }
 
-func NewPassword(pw string) Password {
-	return Password(pw)
+func NewPassword(plaintext string) Password {
+	return Password{
+		plaintext:  plaintext,
+		ciphertext: "",
+	}
 }
