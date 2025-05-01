a8fc2ed02f5d0bcb8b052d53a0d81756b4c9a99d
HEX9CF <hex9cf@aliyun.com>
2025-05-01 19:14:03 +0800

    refactor: update TestMain to change working directory before initializing config

--- test/unit/domain/user_test.go
+++ test/unit/domain/user_test.go
@@ -17,10 +17,18 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	err := config.InitConfig()
+	var err error
+
+	err = os.Chdir("../../..")
+	if err != nil {
+		panic("切换工作目录失败：" + err.Error())
+	}
+
+	err = config.InitConfig()
 	if err != nil {
 		panic("配置文件加载失败：" + err.Error())
 	}
+
 	err = repository.InitDatabase()
 	if err != nil {
 		panic("数据库连接失败：" + err.Error())
