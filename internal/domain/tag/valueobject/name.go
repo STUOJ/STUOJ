bad9dc9f0656fd74e0aafdd20512bc84f00ae5a3
HEX9CF <hex9cf@aliyun.com>
2025-05-03 22:46:20 +0800

    refactor: replace string field with Valueobject in Name type

--- internal/domain/tag/valueobject/name.go
+++ internal/domain/tag/valueobject/name.go
@@ -1,28 +1,27 @@
 package valueobject
 
 import (
+	"STUOJ/internal/model"
 	"STUOJ/pkg/errors"
 	"strings"
 )
 
 type Name struct {
-	value string
+	model.Valueobject[string]
 }
 
 func (n Name) Verify() error {
-	if strings.TrimSpace(n.value) == "" {
+	if strings.TrimSpace(n.Value()) == "" {
 		return errors.ErrValidation.WithMessage("标签名不能为空")
 	}
-	if len(n.value) > 50 {
+	if len(n.Value()) > 50 {
 		return errors.ErrValidation.WithMessage("标签名不能超过50个字符")
 	}
 	return nil
 }
 
-func (n Name) String() string {
-	return n.value
-}
-
 func NewName(value string) Name {
-	return Name{value: value}
+	var n Name
+	n.Set(value)
+	return n
 }
