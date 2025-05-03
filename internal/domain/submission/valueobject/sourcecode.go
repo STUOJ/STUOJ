063f083d8963b6ec8d4fa8b7672790d7454b46af
HEX9CF <hex9cf@aliyun.com>
2025-05-03 22:49:36 +0800

    refactor: replace string type with Valueobject in SourceCode

--- internal/domain/submission/valueobject/sourcecode.go
+++ internal/domain/submission/valueobject/sourcecode.go
@@ -1,22 +1,23 @@
 package valueobject
 
 import (
+	"STUOJ/internal/model"
 	"errors"
 )
 
-type SourceCode string
+type SourceCode struct {
+	model.Valueobject[string]
+}
 
 func (s SourceCode) Verify() error {
-	if len(s) == 0 {
+	if len(s.Value()) == 0 {
 		return errors.New("源代码不能为空！")
 	}
 	return nil
 }
 
-func (s SourceCode) String() string {
-	return string(s)
-}
-
 func NewSourceCode(sourceCode string) SourceCode {
-	return SourceCode(sourceCode)
+	var s SourceCode
+	s.Set(sourceCode)
+	return s
 }
