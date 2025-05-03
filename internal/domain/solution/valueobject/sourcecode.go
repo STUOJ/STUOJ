14a245f754724da3ba22c9859db7e194364edd97
HEX9CF <hex9cf@aliyun.com>
2025-05-03 22:50:46 +0800

    refactor: replace string field with Valueobject in SourceCode

--- internal/domain/solution/valueobject/sourcecode.go
+++ internal/domain/solution/valueobject/sourcecode.go
@@ -1,28 +1,27 @@
 package valueobject
 
 import (
+	"STUOJ/internal/model"
 	"STUOJ/pkg/errors"
 	"strings"
 )
 
 type SourceCode struct {
-	value string
+	model.Valueobject[string]
 }
 
 func (s SourceCode) Verify() error {
-	if strings.TrimSpace(s.value) == "" {
+	if strings.TrimSpace(s.Value()) == "" {
 		return errors.ErrValidation.WithMessage("源代码不能为空")
 	}
-	if len(s.value) > 100000 {
+	if len(s.Value()) > 100000 {
 		return errors.ErrValidation.WithMessage("源代码不能超过100000个字符")
 	}
 	return nil
 }
 
-func (s SourceCode) String() string {
-	return s.value
-}
-
 func NewSourceCode(value string) SourceCode {
-	return SourceCode{value: value}
+	var s SourceCode
+	s.Set(value)
+	return s
 }
