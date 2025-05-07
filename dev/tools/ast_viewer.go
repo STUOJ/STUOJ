dc35a05382cb7d1a263c3ccb7c57bfd16bb6e610
ArtDragonXoX <1262922587@qq.com>
2025-05-07 20:28:23 +0800

    refactor(ast_viewer): 重构AST打印功能以使用标准库方法
    移除自定义的`printAST`函数，改为使用`ast.Print`来打印AST结构，以提高代码的简洁性和可维护性。同时，将`fset`参数添加到`saveASTToFile`函数中，以便正确打印文件位置信息。

--- dev/tools/ast_viewer.go
+++ dev/tools/ast_viewer.go
@@ -16,66 +16,8 @@ import (
 	"path/filepath"
 )
 
-// parseFile 解析指定Go文件并返回AST
-func parseFile(filename string) (*ast.File, error) {
-	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
-	if err != nil {
-		return nil, fmt.Errorf("解析文件失败: %v", err)
-	}
-	return file, nil
-}
-
-// printAST 打印AST结构到控制台
-func printAST(node ast.Node, indent string) {
-	fmt.Printf("%s%T\n", indent, node)
-	if node == nil {
-		return
-	}
-
-	switch n := node.(type) {
-	case *ast.File:
-		fmt.Printf("%s  Name: %s\n", indent, n.Name)
-		for _, decl := range n.Decls {
-			printAST(decl, indent+"  ")
-		}
-	case *ast.GenDecl:
-		fmt.Printf("%s  Tok: %s\n", indent, n.Tok)
-		for _, spec := range n.Specs {
-			printAST(spec, indent+"  ")
-		}
-	case *ast.FuncDecl:
-		fmt.Printf("%s  Name: %s\n", indent, n.Name)
-		printAST(n.Type, indent+"  ")
-	case *ast.FuncType:
-		if n.Params != nil {
-			printAST(n.Params, indent+"  ")
-		}
-		if n.Results != nil {
-			printAST(n.Results, indent+"  ")
-		}
-	case *ast.FieldList:
-		for _, field := range n.List {
-			printAST(field, indent+"  ")
-		}
-	case *ast.Field:
-		if len(n.Names) > 0 {
-			fmt.Printf("%s  Names: %v\n", indent, n.Names)
-		}
-		printAST(n.Type, indent+"  ")
-	case *ast.Ident:
-		fmt.Printf("%s  Name: %s\n", indent, n.Name)
-	case *ast.StructType:
-		printAST(n.Fields, indent+"  ")
-	case *ast.InterfaceType:
-		printAST(n.Methods, indent+"  ")
-	default:
-		fmt.Printf("%s  %#v\n", indent, n)
-	}
-}
-
 // saveASTToFile 将AST结构保存到文件
-func saveASTToFile(filename string, node ast.Node) error {
+func saveASTToFile(filename string, fset *token.FileSet, node ast.Node) error {
 	file, err := os.Create(filename)
 	if err != nil {
 		return fmt.Errorf("创建文件失败: %v", err)
@@ -87,7 +29,7 @@ func saveASTToFile(filename string, node ast.Node) error {
 	os.Stdout = file
 	defer func() { os.Stdout = oldStdout }()
 
-	printAST(node, "")
+	ast.Print(fset, node)
 	return nil
 }
 
@@ -96,17 +38,18 @@ func main() {
 	flag.Parse()
 
 	filename := flag.Arg(0)
-	file, err := parseFile(filename)
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// 处理输出
 	outputFile := filepath.Base(filename) + ".ast"
-	err = saveASTToFile(outputFile, file)
+	err = saveASTToFile(outputFile, fset, file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("AST已保存到: %s\n", outputFile)
-	printAST(file, "")
+	ast.Print(fset, file)
 }
