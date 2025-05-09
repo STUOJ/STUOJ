d3621d562113a13b03e49452f03a93d2e96fd325
ArtDragonXoX <1262922587@qq.com>
2025-05-09 23:32:12 +0800

    feat(album): 添加创建专辑和添加格式功能
    新增了 `CreateAlbum` 和 `AlbumAddFormat` 函数，用于创建专辑和向专辑中添加格式。这些功能通过 HTTP 请求与后端交互，并处理 JSON 数据的编码和解码。

--- internal/infrastructure/yuki/album.go
+++ internal/infrastructure/yuki/album.go
@@ -1,6 +1,7 @@
 package yuki
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 
@@ -54,4 +55,58 @@ func GetAlbum(albumId uint64) (YukiAlbum, error) {
 		return YukiAlbum{}, err
 	}
 	return album, nil
+}
+
+func CreateAlbum(name string, height, width int64) (YukiAlbum, error) {
+	data := map[string]interface{}{
+		"name":   name,
+		"height": height,
+		"width":  width,
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return YukiAlbum{}, err
+	}
+	bodystr, err := httpInteraction("/album", "POST", bytes.NewReader(jsonData))
+	if err != nil {
+		return YukiAlbum{}, err
+	}
+	var responses YukiResponses
+	err = json.Unmarshal([]byte(bodystr), &responses)
+	if err != nil {
+		return YukiAlbum{}, err
+	}
+	if responses.Code == 0 {
+		return YukiAlbum{}, errors.New(responses.Message)
+	}
+	var album YukiAlbum
+	err = mapstructure.Decode(responses.Data, &album)
+	if err != nil {
+		return YukiAlbum{}, err
+	}
+	return album, nil
+}
+
+func AlbumAddFormat(album uint8, format uint64) error {
+	data := map[string]interface{}{
+		"format_name": GetFormatName(format),
+		"album_name":  GetAlbumName(album),
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	bodystr, err := httpInteraction("/album/format", "POST", bytes.NewReader(jsonData))
+	if err != nil {
+		return err
+	}
+	var responses YukiResponses
+	err = json.Unmarshal([]byte(bodystr), &responses)
+	if err != nil {
+		return err
+	}
+	if responses.Code == 0 {
+		return errors.New(responses.Message)
+	}
+	return nil
 }
