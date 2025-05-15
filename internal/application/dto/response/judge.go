6f2bea22277b1e4b1cfbbce4af4d25970ba5670c
ArtDragonXoX <1262922587@qq.com>
2025-05-15 16:09:59 +0800

    feat: 在TestRunRes结构体中添加Stderr和CompileOutput字段
    为了支持更详细的运行结果信息，在TestRunRes结构体中新增了Stderr和CompileOutput字段，以便捕获标准错误和编译输出信息。

--- internal/application/dto/response/judge.go
+++ internal/application/dto/response/judge.go
@@ -3,17 +3,21 @@ package response
 import "STUOJ/internal/domain/runner"
 
 type TestRunRes struct {
-	Memory int64   `json:"memory"`
-	Status int64   `json:"status"`
-	Stdout string  `json:"stdout"`
-	Time   float64 `json:"time"`
+	Memory        int64   `json:"memory"`
+	Status        int64   `json:"status"`
+	Stdout        string  `json:"stdout"`
+	Time          float64 `json:"time"`
+	Stderr        string  `json:"stderr"`
+	CompileOutput string  `json:"compile_output"`
 }
 
 func Result2TestRunRes(result runner.RunnerResult) TestRunRes {
 	return TestRunRes{
-		Memory: int64(result.Memory),
-		Status: int64(result.Status.Id),
-		Stdout: result.TestResult[0].Stdout,
-		Time:   result.Time,
+		Memory:        int64(result.Memory),
+		Status:        int64(result.Status.Id),
+		Stdout:        result.TestResult[0].Stdout,
+		Time:          result.Time,
+		Stderr:        result.TestResult[0].Stderr,
+		CompileOutput: result.CompileOutput,
 	}
 }
